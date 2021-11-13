4dec502d137bde39a3b923154b0cbff9a29d04a1
Anreeta <asaseetharran@g.ucla.edu>
2021-11-12 20:30:08 -0800

    Add documentation/comments to GameManager object

--- Unity/Assets/Scripts/GameManager.cs
+++ Unity/Assets/Scripts/GameManager.cs
@@ -3,21 +3,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/* GameManager object created by adapting the tutorial provided here:
+    https://www.youtube.com/watch?v=4I0vonyqMi8
+
+*/
+
 public class GameManager : MonoBehaviour
-{
+{   
+    // Use this Instance variable to reference the GameManager object in other scripts
+    // E.g. can call GameManager.Instance.UpdateGameState(state variable) to update states
     public static GameManager Instance;
 
+    // Variable keeps track of GameState
     public GameState State;
 
+    // Use this event to have other scripts be alerted when GameState changes
     public static event Action<GameState> OnGameStateChanged;
 
+    // Initialize variable before program starts
     void Awake()
     {
         Instance = this;
     }
 
+    // Set state on game start
     void Start() {
-        UpdateGameState(GameState.SelectLevel);
+        UpdateGameState(GameState.StartScreen);
     }
 
     // Update is called once per frame
@@ -33,11 +44,16 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    // Function that is invoked whenever game state is changed
     public void UpdateGameState(GameState newState)
     {
         State = newState;
 
         switch(newState) {
+            case GameState.StartScreen:
+                break;
+            case GameState.MainMenu:
+                break;
             case GameState.SelectLevel:
                 // HandleSelectLevel();
                 break;
@@ -53,13 +69,17 @@ public class GameManager : MonoBehaviour
                 throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
         }
 
+        // If other scripts are suscribed to changes in GameState, inform them
         OnGameStateChanged?.Invoke(newState);
     }
 
 
 }
 
+// enumeration of all possible game states
 public enum GameState {
+    StartScreen,
+    MainMenu,
     SelectLevel,
     Playing,
     Paused,
