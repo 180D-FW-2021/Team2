d58766a8bdee509bee5f92f0fb2893ab9e1a3626
Anreeta <asaseetharran@g.ucla.edu>
2022-01-28 15:40:36 -0800

    Added movenet builder for Windows

---
+++ Setup_Scripts/build_movenet.bat
@@ -0,0 +1,12 @@
+:: Build movenet executable to execute from Unity
+:: Ensure to activate conda env before running
+:: This may take a few mins...
+set MOVENET_DIR=..\Movenet\src
+cd %MOVENET_DIR%
+
+:: build single executable with all dependencies installed
+pyinstaller -F --onefile position_tracking.py
+:: executable must be in directory Movenet/src
+:: due to relative model paths
+cd dist
+move position_tracking ..
