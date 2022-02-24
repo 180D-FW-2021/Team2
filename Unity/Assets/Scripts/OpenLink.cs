ed7aa7c801e9d0a8249e5465c523aff6c40782c1
Anreeta <asaseetharran@g.ucla.edu>
2022-02-24 00:25:17 -0800

    Add View Instructions Online button to Help Menu

---
+++ Unity/Assets/Scripts/OpenLink.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    // Open the Instructions in the Webapp
+    public void OpenWebappInstructions()
+    {
+        Application.OpenURL("https://amaze-webapp.herokuapp.com/instructions");
+    }
+
+    // Open the Leaderboard in the Webapp
+    public void OpenWebappLeaderboard()
+    {
+        Application.OpenURL("https://amaze-webapp.herokuapp.com/leaderboard");
+    }
+}
