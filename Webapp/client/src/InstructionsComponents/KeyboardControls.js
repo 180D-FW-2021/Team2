dd55028c7856b3ac70aec33c3df7870c631f8cb1
Sydney <sydneylaiw@gmail.com>
2022-02-25 20:49:56 -0800

    use image for keyboard controls

--- Webapp/client/src/InstructionsComponents/KeyboardControls.js
+++ Webapp/client/src/InstructionsComponents/KeyboardControls.js
@@ -1,45 +1,14 @@
-import BootstrapTable from "react-bootstrap-table-next";
-
-const columns = [
-  {
-    dataField: "control",
-    text: "Control",
-  },
-  {
-    dataField: "key",
-    text: "Key(s)",
-  },
-];
-
-const data = [
-  {
-    control: "Player movement",
-    key: "Arrow keys or WASD",
-  },
-  {
-    control: "Rotate camera",
-    key: "'z' (left), 'x' (right)",
-  },
-  {
-    control: "Jump",
-    key: "Space bar",
-  },
-  {
-    control: "Duck",
-    key: "Left shift",
-  },
-  {
-    control: "Open pause menu",
-    key: "ESC",
-  },
-];
-
 function KeyboardControls() {
   return (
     <div>
       <h3>Keyboard Controls</h3>
       <p>
-        <BootstrapTable keyField="control" columns={columns} data={data} />
+        <img
+          id="key-controls"
+          src="/images/keyboard-controls.png"
+          alt="keyboard controls"
+          width="400"
+        />
       </p>
     </div>
   );
